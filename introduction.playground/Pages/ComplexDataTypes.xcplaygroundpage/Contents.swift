599d43f326e9e01262904d6e582c238be832bc3e
YagoAres <yago.ares.armesto@accenture.com>
2024-06-17 11:38:43 +0200

    Arrays + sets (fix)

--- introduction.playground/Pages/ComplexDataTypes.xcplaygroundpage/Contents.swift
+++ introduction.playground/Pages/ComplexDataTypes.xcplaygroundpage/Contents.swift
@@ -1,7 +1,33 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
+//ARRAYS
+let yago = "Yago Ares"
+let jesus = "Jesús Ares"
+let mama = "Ana Armesto"
 
-//: [Next](@next)
+let miFamilia = [yago, jesus, mama]
+print(miFamilia[1])
+
+//También puedes especificar el tipo de dato, en esta caso un array de strings
+var cities: [String] = ["London" , "Paris", "New York", "Praga"]
+
+//Array enteros
+var scores: [Int] = [10, 12, 9]
+
+//Array de boleanos
+var readings: [Bool] = [false, false, true, false]
+//Y así con más tipos de datos
+//Recordar que no hace falta especificar el tipo de dato
+
+//CONJUNTOS/SETS
+//No se almacenan en ningún orden, es decir en un orden aleatorio y todos los elementos debes ser únicos
+let colors = Set(["red", "green", "blue", "yellow"])
+print(colors) //Si observas en las salida ves que no coincide con el orden que yo cree, swift no ofrece garantías de su orden
+
+//Si por ejemplo insertamos un elemento duplicado, estos se ignorar
+let colors2 = Set(["red", "green", "red", "blue"])
+print(colors2)
+
+//Conjuntos con enteros
+let numeros = Set([1,2,2,3,4,3])
+print(numeros)
--- introduction.playground/Pages/Variables general.xcplaygroundpage/Contents.swift
+++ introduction.playground/Pages/Variables general.xcplaygroundpage/Contents.swift
@@ -3,7 +3,6 @@ import UIKit
 //Variables
 
 //Una vez que la variable se declara, se puede cambiar tantas veces como se quiera, quedándose con el último valor declarado, siempre que sea el mismo tipo de dato
-
 var message = "Hello, playground"
 message = "Hola mundo"
 message = "Último mensaje"
@@ -23,33 +22,22 @@ print(varibleInt)
 var variableFloat: Float = 10.0
 
 //Se pueden declarar varaible en la misma linea
-var cordenadaX = 0.0, cordenadaY = 0.0, cordenadaZ = 0.0
+var cordenatesX = 0.0, cordenatesY = 0.0, cordenatesZ = 0.0
 
 //Constantes, no se le puedes modificar la variable
 
-let constante = "Swift"
+let constant = "Swift"
 //constante = "Hola"
 
 //Podemos crear alias
 typealias Name = String
 let username : Name = "Yago Ares"
 
-//Tuplas
-let user = ("Hola","Yago")
-
-print(user.0)
-
-let (firstValue, secondValue) = ("Primero", "Segundo ")
-print(firstValue)
-
 //Interpolación
-
 var score = 85
 var str = "Your score was\(score)"
  
- 
 //Anotaciones, especificar el tipo de datos que quieres
-
 let message1 = "Hello, playground"
 let album: String = "Reputation"
 let year: Int = 2024
