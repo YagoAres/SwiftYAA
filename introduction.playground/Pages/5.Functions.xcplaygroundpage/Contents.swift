3c80f2d7069f225e5b97fe27d43f6c1148e45578
YagoAres <yago.ares.armesto@accenture.com>
2024-06-19 16:47:08 +0200

    Continuando con funciones y parámetros

--- introduction.playground/Pages/5.Functions.xcplaygroundpage/Contents.swift
+++ introduction.playground/Pages/5.Functions.xcplaygroundpage/Contents.swift
@@ -20,7 +20,6 @@ square(number: 10)
 
 //3. RETURN
 //Además de recibir datos, las funciones también pueden enviar datos, por ejemplo
-
 func square2(number: Int) -> Int {
     return number * number
 }
@@ -30,10 +29,64 @@ let result = square2(number: 8)
 print(result)
 
 //5. Parameter labels
-
 //Swift nos permite proporcionar dos nombres para cada parámetro, uno para llamar externamente a la función y otro internamente, tan sencillo como:
-
 func sayHello(to name: String){
     print("Hello, \(name)!")
 }
  sayHello(to: "Yago")
+
+//6. OMITTING PARAMETERS LABEL
+//Se puede obtener el mismo comportamiento en sus propias funciones usando un guión bajo, _
+
+func greet(_ person: String) {
+    print("Hello, \(person)!")
+}
+
+greet("Josh")
+
+//7. DEFAULT PARAMETERS
+//Puede darle a sus propios parámetros un valor predeterminado simplemente escribiendo un =
+func greet(_ person: String, nicely: Bool = true) {
+    if nicely == true {
+        print("Hello, \(person)!")
+    } else {
+        print("Oh no, it's \(person) again...")
+    }
+}
+//Eso se puede llamar de dos maneras:
+greet("Taylor")
+greet("Taylor", nicely: false)
+
+
+//8. VARIADIC FUNCTIONS
+//Se pueden pasar varias variables en una sola función
+func square2(numbers: Int...) {
+for number in numbers {
+    print("\(number) squared is \(number * number)")
+ }
+}
+
+square2(numbers: 1, 2, 3, 4, 5) //
+
+//9. THROWING FUNCTIONS
+// Swift nos permite arrojar errores de funciones marcándolas como throwsantes de su tipo de retorno y luego usando la palabra throw cuando algo sale mal
+enum PasswordError: Error {
+    case obvious
+}
+
+func checkPassword(_ password: String) throws -> Bool {
+    if password == "password" {
+        throw PasswordError.obvious
+    }
+
+    return true
+}
+
+//10. RUNNING THROWING FUNCTIONS
+//A Swift no le gusta que se produzcan errores cuando se ejecuta su programa, lo que significa que no le permitirá ejecutar una función que genere errores por accidente.
+do {
+    try checkPassword("password")
+    print("That password is good!")
+} catch {
+    print("You can't use that password.")
+}
